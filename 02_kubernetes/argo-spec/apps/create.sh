5ae6c5a3575a6d89b8fe50c2fe2357de1f368b3b
Pascal Brokmeier <pascal_brokmeier@mckinsey.com>
2021-05-28 14:25:53 +0200

    fix path

--- 02_kubernetes/argo-spec/apps/create.sh
+++ 02_kubernetes/argo-spec/apps/create.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+set -xe
 argocd app create apps \
     --dest-namespace argo-cd \
     --dest-server https://kubernetes.default.svc \
     --repo https://github.com/pascalwhoop/azure-datalake \
-    --path 02_kubernetes/apps  
+    --path 02_kubernetes/argo-spec/apps
 argocd app sync apps  
