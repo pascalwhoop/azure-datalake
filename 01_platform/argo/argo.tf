e9b5bdedb703b98de65dbb7d6aa8aa025824f227
Pascal Brokmeier <pascal_brokmeier@mckinsey.com>
2021-05-30 12:34:26 +0200

    argocd namespace rename

--- 01_platform/argo/argo.tf
+++ 01_platform/argo/argo.tf
@@ -1,17 +1,28 @@
 resource "kubernetes_namespace" "argo" {
   metadata {
     annotations = {
-      name = "argo-cd"
+      name = "argocd"
     }
 
-    name = "argo-cd"
+    name = "argocd"
   }
 }
 
 resource "helm_release" "argo" {
     name = "argo"
     repository = "https://argoproj.github.io/argo-helm"
-    chart = "argo-cd"
+    chart = "argocd"
     namespace = kubernetes_namespace.argo.metadata[0].name
+
+    # pass through ssl to enable grpc/https for argocd CLI, see
+    # https://argoproj.github.io/argo-cd/operator-manual/ingress/#kubernetesingress-nginx
+    set {
+        name = "server.ingress.annotations"
+        value = {
+            "kubernetes.io/ingress.class": "nginx",
+            "nginx.ingress.kubernetes.io/force-ssl-redirect": "true",
+            "nginx.ingress.kubernetes.io/ssl-passthrough": "true",
+        }
+    }
   
 }
